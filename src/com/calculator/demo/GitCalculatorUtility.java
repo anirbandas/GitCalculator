dac74eb1240f30de0f23a02be84d783cdf3260bb
anirbandas <anirbandas_ju@yahoo.co.in>
2013-09-24 14:43:08 +0530

    initial commit for calculator utility tool

---
+++ src/com/calculator/demo/GitCalculatorUtility.java
@@ -0,0 +1,13 @@
+package com.calculator.demo;
+
+public class GitCalculatorUtility {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("Welcome to new calculator class");
+	}
+
+}
